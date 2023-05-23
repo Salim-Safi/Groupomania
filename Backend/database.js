20003193756f35a567b3088e10d8d1942e724d37
Salim <salimbeni.safi@gmail.com>
2023-05-23 19:22:35 +0200

    Back: maj fichier database

--- Backend/database.js
+++ Backend/database.js
@@ -8,14 +8,3 @@ mongoose
   })
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch((error) => console.log("Connexion à MongoDB échouée !", error));
-
-const db = mongoose.connection;
-
-db.on("error", (error) =>
-  console.error("Erreur de connexion MongoDB :", error)
-);
-db.once("open", () =>
-  console.log("Connexion à la base de données MongoDB établie.")
-);
-
-module.exports = db;
